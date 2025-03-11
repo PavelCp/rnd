684b7e8488a101c8f99b9934df2e745da6c1fa21
Pavel C <pavelcapet@gmail.com>
2025-03-11 14:17:19 +0200

    Updates

---
+++ assets/Button.stories-cAqJQKIS.js
@@ -0,0 +1,25 @@
+import{f as k}from"./index-DZ0MIZXx.js";import{B as z}from"./Button-Brdg18Zs.js";import"./jsx-runtime-D_zvdyIk.js";const E={title:"Example/Button",component:z,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:k()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}},s={args:{primary:!1,label:"Button"}};var t,n,c;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var l,m,p;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var u,i,d;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(d=(i=e.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var g,y,b;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(b=(y=o.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var B,S,f;s.parameters={...s.parameters,docs:{...(B=s.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: "Button"
+  }
+}`,...(f=(S=s.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};const L=["Primary","Secondary","Large","Small","NewStorybookStory"];export{e as Large,s as NewStorybookStory,r as Primary,a as Secondary,o as Small,L as __namedExportsOrder,E as default};
